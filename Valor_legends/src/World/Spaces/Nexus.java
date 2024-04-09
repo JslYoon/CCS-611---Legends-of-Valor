8eded0552de6019a7e8a2505a4ae401ad36af9a8
Lucas <junsuny789@gmail.com>
2024-04-09 15:54:59 -0400

    add nexus

---
+++ Valor_legends/src/World/Spaces/Nexus.java
@@ -0,0 +1,71 @@
+package src.World.Spaces;
+
+import java.util.ArrayList;
+
+import src.Behaviors.Behaviors;
+import src.Entities.Entities;
+import src.Entities.Enemies.Monsters;
+import src.Entities.Players.Heros;
+import src.Entities.Players.Party;
+import src.World.Coordinate;
+
+// java object to indicate common tile
+
+public class Nexus implements Spaces{
+
+    private Party occupied;
+    private Coordinate coord = null;
+
+    public Nexus() {
+        occupied = null;
+    }
+
+    public Nexus(Party p) {
+        occupied = p;
+    }
+
+    public void setOccupied(Party p) { occupied = p; }
+    public Party getOccupied() { return occupied; }
+
+    public void beginAction(Party p) {
+
+        System.out.println("You met enemies");
+        Behaviors.PlayerVsMonster(p);
+
+    }
+
+    public String spaceType() {
+        return "Common";
+    }
+
+    public boolean isPartyHere(){
+        if(occupied != null) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean canMoveto() {
+        return true;
+    }
+
+
+     public void setCoord(Coordinate c) {
+        coord = c;
+    }
+
+    public Coordinate getCoord() { return coord; }
+
+ 
+    public String toString() {
+        String s = "";
+        s += "|\t" + "common" + "\t|";
+        return s;
+    }
+    
+    public String[] getRepr() {
+        String[] r =  {"|\t \t|", "|\t\t|", "|\t\t|"};
+        return r;
+    }
+
+}
