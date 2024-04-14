c7edffa8a0d2fd3282cd3557a77ea1bdb42d6e72
Lucas <junsuny789@gmail.com>
2024-04-14 15:04:33 -0400

    change world interface interaction

--- Valor_legends/src/Misc/Interface.java
+++ Valor_legends/src/Misc/Interface.java
@@ -1,6 +1,7 @@
 package src.Misc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import src.Entities.Entities;
 import src.Entities.Players.Heros;
@@ -17,7 +18,11 @@ public class Interface {
     private ValorWorld valorWorld;
     private int world_r = 8;
     private int world_c = 8;
+    private int valor_lanes = 2;
+    private int valor_lane_size = 2;
     private Party myParty = null;
+    private HashMap<Integer, Party> ValorParty = null;
+
     private ArrayList<String> Roles = new ArrayList<>();
     
     public Interface() {
@@ -62,16 +67,15 @@ public class Interface {
                                 System.out.println("Goodbye!");
                                 return;
                             case 1:
-                                //TODO need to change creating party for legends of valor 
-                                Party p = myParty; 
-                                if(myParty == null) {
-                                    p = createParty();
-                                    myParty = p;
+                                HashMap<Integer, Party> p = ValorParty; 
+                                if(p == null) {
+                                    p = createValorParty();
+                                    ValorParty = p;
                                 }
                                 if (p == null){
                                     continue;
                                 }
-                                startValorGame(p);
+                                startValorGame();
                                 break;
                             case 2:
                                 createValorWorld();
@@ -82,40 +86,48 @@ public class Interface {
                     }
             }
         }
-        
     }
+
 
     private void startMonsterGame(Party p) {
         world = new MonsterWorld(world_r, world_c, p);
         while(true) {
-            System.out.println(valorWorld);
-            int nums = playerInputs(p);
+            System.out.println(world);
+            int nums = playerInputs(p, 'M');
             if (nums == -1) {
                 return;
             }
         }
     }
 
-    private void startValorGame(Party p) {
-        valorWorld = new ValorWorld(world_r, p, 2, 2);
+    private void startValorGame() {
+        System.out.println();
+        valorWorld = new ValorWorld(8, ValorParty, valor_lanes, valor_lane_size);
         while(true) {
-            System.out.println(valorWorld);
-            int nums = playerInputs(p);
-            if (nums == -1) {
-                return;
+            for(int i = 0; i < ValorParty.size(); i++) {
+                Party currparty = ValorParty.get(0);
+                System.out.println(valorWorld);
+                System.out.println(currparty.getPartyMembers().get(0).getName() + "'s turn");
+                int nums = playerInputs(currparty, 'V');
+                if (nums == -1) {
+                    return;
+                }
+                
             }
         }
     }
 
     // Player input interface for the actual game
-    private int playerInputs(Party p) {
-
+    private int playerInputs(Party p, char c) {
         int playerEntry = printStatement.playerMoveOption(world.currPartySpace());
         switch(playerEntry) {
             case 0:
                 return -1;
             case 1: // Move party
                 world.moveParty();
+                if(c == 'V'){
+                    // TODO: check for warp and battle
+                }
                 uponplayerMove(p);
                 return 1;
             case 2: // View inventory
@@ -129,6 +141,8 @@ public class Interface {
         }
         return -1;
     }
+
+
 
     // Is a method to be called after a player moves
     private void uponplayerMove(Party p) {
@@ -188,6 +202,23 @@ public class Interface {
         return party;
     }
 
+    // create and return valor party
+    public HashMap<Integer, Party>  createValorParty() {
+        HashMap<Integer, Party> tempmap = new HashMap<>();
+        ArrayList<Entities> partiers = new ArrayList<>();
+        for(int i = 0; i < valor_lanes; i++) {
+            System.out.println("Creating hero for lane " + (i+1));
+            Entities en = createHero();
+            if(en == null) { return null; }
+            partiers.add(en);
+            Party party = new Party(partiers);
+            tempmap.put(i, party);
+        }
+        
+        return tempmap;
+
+    }
+
 
     private void createMonsterWorld() {
         System.out.println("======================");
@@ -205,14 +236,14 @@ public class Interface {
     private void createValorWorld() {
         // TODO change this to take in column size, row number, number of lanes
         System.out.println("======================");
-        System.out.println("Choose your world row size (8 ~ 16)");
-        int num1 = Input.untilNumberInput(8, 16);
+        System.out.println("How many lanes would you want? (1 ~ 3)");
+        int num1 = Input.untilNumberInput(1, 3);
         if (num1 == -1) {return;}
-        System.out.println("Choose your world column size (8 ~ 16)");
-        int num2 = Input.untilNumberInput(8, 16);
+        System.out.println("What size should the lines be (1 ~ 3)");
+        int num2 = Input.untilNumberInput(1, 3);
         if (num2 == -1) {return;}
-        world_r = num1;
-        world_c = num2;
+        valor_lanes = num1;
+        valor_lane_size = num2;
     }
 
     // create hero
@@ -227,7 +258,13 @@ public class Interface {
         
         System.out.println("Hello " + name + "! Choose your hero role (Warrior, Paladin, Sorcerer)");
         String s1 = Input.untilGivenStringInput(Roles);
-        Heros h = new Heros(name, s1);
+        Heros h  = new Heros(name, s1);;
+        switch(s1) {
+            case "Warrior":
+                h = new Heros(name, s1);
+        }
+      
+
         System.out.println("You chose " + s1 + ". Enjoy the game.");
         System.out.println("======================");
         return h;
